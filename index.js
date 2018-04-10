52aa97381009c6df491bb133c1f47592bc807630
tj11 Johnston <tedwardj11@gmail.com>
2018-04-10 00:16:50 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  //document.getElementsByTagName('div')
+  return document.getElementById('nested').querySelector('div')
+}
+
+function increaseRankBy(n) {
+  var ranks = document.querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = ranks[i].innerHTML+5;
+  }
+  return ranks
+}
