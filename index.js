e6a1984168c22968757ba8e16f97e4bf0b542cac
tj11 Johnston <tedwardj11@gmail.com>
2018-04-10 00:59:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  //document.getElementsByTagName('div')
+  return document.getElementById('nested').querySelector('div')
+}
+
+function increaseRankBy(n) {
+  //var ranks = document.querySelectorAll('ul.ranked-list li');
+  //for (let i = 0; i < ranks.length; i++) {
+  //  ranks[i].innerHTML = ranks[i].innerHTML+5;
+  //}
+  //return ranks
+  
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML)+n
+  }
+  
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div')
+}
